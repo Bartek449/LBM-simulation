e423f200501075a96b15983ee06f4157be72b994
bartek <bbszwarga@gmail.com>
2024-12-16 19:55:53 +0100

    zmiana w nazewnictwie

--- Cell.cpp
+++ Cell.cpp
@@ -1,32 +1,32 @@
 #include"Cell.h"
 
-Cell::Cell() : info(EMPTY) {}
+Cell::Cell() : fun_in(EMPTY) {}
 
-Cell::Cell(array<double,4> i) {info = i;}
+Cell::Cell(array<double,4> i) { fun_in = i;}
 
-void Cell::set_info(array<double,4> i) {info = i; }
+void Cell::set_fun_in(array<double,4> i) { fun_in = i; }
 
-const array<double,4>& Cell::get_info() const { return info; }
+const array<double,4>& Cell::get_fun_in() const { return fun_in; }
 
 double Cell::calculate_density()
 {
     double p = 0.0;
-    for (const auto& val : info)
+    for (const auto& val : fun_in)
     {
         p += val;
     }
     return p;
 }
 
-double Cell::calculate_fun_eq() { return calculate_density() / info.size(); }
+double Cell::calculate_fun_eq() { return calculate_density() / fun_in.size(); }
 
 array < double,4> Cell::calculate_fun_exit() 
 { 
     array <double, 4> f_ex;
     double f_eq = calculate_fun_eq();
     for (int i = 0; i < 4; ++i) {
-        f_ex[i] = info[i] + (1.0 / RELAVATION_TIME) * (f_eq - info[i]); 
+        f_ex[i] = fun_in[i] + (1.0 / RELAVATION_TIME) * (f_eq - fun_in[i]);
      }
 
-        return f_ex;
+     return f_ex;
 }
