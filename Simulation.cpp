eb6d8f17da86818934c262625c5707e3cfc3f9a8
bartek <bbszwarga@gmail.com>
2024-12-17 08:52:31 +0100

    lga to lbm ciąg dalszy

---
+++ Simulation.cpp
@@ -0,0 +1,88 @@
+#include "Simulation.h"
+
+Simulation::Simulation(int rows, int columns) : s(rows, columns) {}
+
+Matrix& Simulation::get_matrix() { return s; }
+
+void Simulation::collision() {
+    Matrix next_matrix = s;
+    int rows = s.get_rows_num();
+    int columns = s.get_columns_num();
+    vector<int> left_right{ 1, 0, 1, 0 };
+    vector<int> up_down{ 0, 1, 0, 1 };
+
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < columns; j++) {
+            const array<double,4>& info = s.get_element(i, j).get_fun_in();
+            Cell& future_cell = next_matrix.get_element(i, j);
+
+           
+
+        }
+    }
+
+    s = next_matrix;
+}
+
+
+void Simulation::streaming() {
+    Matrix next_matrix = s;
+    int rows = s.get_rows_num();
+    int columns = s.get_columns_num();
+
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < columns; j++) {
+            if (s.get_element(i, j).get_fun_in() != WALL) {
+                for (int d = 0; d < 4; d++) {
+                    next_matrix.get_element(i, j).set_direct_fun_in(d, 0);
+                }
+            }
+        }
+    }
+
+    for (size_t i = 1; i < rows - 1; i++) {
+        for (size_t j = 1; j < columns - 1; j++) {
+            Cell& current_cell = s.get_element(i, j);
+            array<double, 4> info = current_cell.get_fun_in();
+
+            /*if (info[0] == 1) {
+                Cell& right_cell = s.get_element(i, j + 1);
+                if (right_cell.get_info() == WALL) {
+                    next_matrix.get_element(i, j).set_direct_info(2, 1);
+                }
+                else {
+                    next_matrix.get_element(i, j + 1).set_direct_info(0, 1);
+                }
+            }
+            if (info[1] == 1) {
+                Cell& down_cell = s.get_element(i + 1, j);
+                if (down_cell.get_info() == WALL) {
+                    next_matrix.get_element(i, j).set_direct_info(3, 1);
+                }
+                else {
+                    next_matrix.get_element(i + 1, j).set_direct_info(1, 1);
+                }
+            }
+            if (info[2] == 1) {
+                Cell& left_cell = s.get_element(i, j - 1);
+                if (left_cell.get_info() == WALL) {
+                    next_matrix.get_element(i, j).set_direct_info(0, 1);
+                }
+                else {
+                    next_matrix.get_element(i, j - 1).set_direct_info(2, 1);
+                }
+            }
+            if (info[3] == 1) {
+                Cell& up_cell = s.get_element(i - 1, j);
+                if (up_cell.get_info() == WALL) {
+                    next_matrix.get_element(i, j).set_direct_info(1, 1);
+                }
+                else {
+                    next_matrix.get_element(i - 1, j).set_direct_info(3, 1);
+                }
+            }*/
+        }
+    }
+
+    s = next_matrix;
+}
---
+++ Simulation.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Matrix.h"
+
+class Simulation {
+private:
+    Matrix s;
+public:
+    Simulation(int, int);
+    Matrix& get_matrix();
+
+    void collision();
+    void streaming();
+};
